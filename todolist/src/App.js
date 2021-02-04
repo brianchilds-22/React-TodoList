31ba27a1bf1df0fd5762d436bdf9d39854060628
brianchilds-22 <brianchilds22@msn.com>
2021-02-04 14:13:08 -0700

    AddedButton

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
 
-function App() {
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'feb 5 at 3',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Dev meeting',
+        day: 'feb 7 at 4',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Drinks',
+        day: 'feb 9 at 7',
+        reminder: true,
+    }
+])
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !==id))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
